aa9714df6bada42e459424531efda6fa0fc3ca03
Brennan Duck <brennantduck@gmail.com>
2023-09-23 16:02:52 -0400

    added basic clicking functionality

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 const scoreButton = document.querySelector("#give-wm");
 
-let scoreDisplay = document.querySelector("#game_score-number").innerHTML;
-
-let score = 0;
+let scoreDisplay = document.querySelector("#game_score-number");
 
 function addScore(amount) {
   score = score + amount;
   scoreDisplay.innerHTML = score;
-  console.log(score);
 }
 
-scoreButton.addEventListener("click", addScore(1));
+let score = 0;
+
+scoreButton.addEventListener("click", () => {
+  addScore(1);
+});
